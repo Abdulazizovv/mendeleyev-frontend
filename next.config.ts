06e20ee9c155c8c01eed6f47809f7964f3e5787d
abdulazizovv <aqllibola567@gmail.com>
2026-06-20 16:36:54 +0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
 
-  const nextConfig: NextConfig = {
-    output: 'standalone',
-    headers: async () => [
-      {
-        source: '/favicon.ico',
-        headers: [{ key: 'Cache-Control', value: 'public, max-age=3600, must-revalidate' }],
-      },
-    ],
-  };
+const nextConfig: NextConfig = {
+  output: "standalone",
+  headers: async () => [
+    {
+      source: "/favicon.ico",
+      headers: [{ key: "Cache-Control", value: "public, max-age=3600, must-revalidate" }],
+    },
+  ],
+};
 
-  export default nextConfig;
+export default nextConfig;
